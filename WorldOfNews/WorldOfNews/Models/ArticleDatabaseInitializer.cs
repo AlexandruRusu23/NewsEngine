17f14beb17fbf76a6f87c1be85d1137acacc6402
AlexandruRusu23 <alexandru.rusu707@gmail.com>
2017-01-08 02:11:31 +0200

    Presentation form

--- WorldOfNews/WorldOfNews/Models/ArticleDatabaseInitializer.cs
+++ WorldOfNews/WorldOfNews/Models/ArticleDatabaseInitializer.cs
@@ -45,6 +45,11 @@ namespace WorldOfNews.Models
                     CategoryID = 5,
                     CategoryName = "Technology"
                 },
+                new Category
+                {
+                    CategoryID = 6,
+                    CategoryName = "Science"
+                }
             };
 
             return categories;
@@ -56,48 +61,48 @@ namespace WorldOfNews.Models
             {
                 new Article
                 {
-                    CategoryID = 5,
-                    ArticleName = "Article number 1",
+                    CategoryID = 2,
+                    ArticleName = "Donald Trump is the new president of the United States",
                     ImagePath = "image1.png",
-                    DatePublished = DateTime.Parse("12/10/2010"),
+                    DatePublished = DateTime.Parse("12/10/2016"),
                     ArticleID = 1,
-                    Description = "Description for article number 1 made by Initializer"
-                },
-                new Article
-                {
-                    CategoryID = 4,
-                    ArticleName = "Article number 2",
-                    ImagePath = "image2.png",
-                    DatePublished = DateTime.Parse("12/17/2016"),
-                    ArticleID = 2,
-                    Description = "Description for article number 2 made by Initializer"
+                    Description = "USA will have to feel the real pressure now that they've changed their president. Barack Obama is feeling awful, but he knows that is better than Donald Trump."
                 },
                 new Article
                 {
                     CategoryID = 3,
-                    ArticleName = "Article number 3",
-                    ImagePath = "image3.png",
-                    DatePublished = DateTime.Parse("1/2/2017"),
+                    ArticleName = "Student nowadays don't read book like past's students",
+                    ImagePath = "image4.png",
+                    DatePublished = DateTime.Parse("12/17/2016"),
                     ArticleID = 3,
-                    Description = "Description for article number 3 made by Initializer"
+                    Description = "The number of student who don't learn the courses from books is decreasing very fast since they've got tablets, laptops and so on. Authorities are alarmed, but me personaly I'm fine."
                 },
                 new Article
                 {
-                    CategoryID = 2,
-                    ArticleName = "Article number 4",
-                    ImagePath = "image4.png",
+                    CategoryID = 4,
+                    ArticleName = "Basketball everywhere",
+                    ImagePath = "image2.png",
+                    DatePublished = DateTime.Parse("1/2/2017"),
+                    ArticleID = 2,
+                    Description = "Number of basketball's players has been increased miraculously in the last year and that's a benefit for the players, but also for equipment shops."
+                },
+                new Article
+                {
+                    CategoryID = 5,
+                    ArticleName = "New iPhone 7 Plus is here and it's awesome",
+                    ImagePath = "image3.png",
                     DatePublished = DateTime.Parse("10/2/2016"),
                     ArticleID = 4,
-                    Description = "Description for article number 4 made by Initializer"
+                    Description = "After Apple has launched their new mobile phones, we were more than enthusiast and get a new matte black iPhone 7 Plus to test in our studio."
                 },
                 new Article
                 {
                     CategoryID = 1,
-                    ArticleName = "Article number 5",
+                    ArticleName = "The wars will not get us anywhere",
                     ImagePath = "image5.png",
                     DatePublished = DateTime.Parse("6/22/2016"),
                     ArticleID = 5,
-                    Description = "Description for article number 5 made by Initializer"
+                    Description = "Countries like Syria are in a dark period because of civil wars and a lot of citizens are in danger of death."
                 }
             };
 
@@ -111,7 +116,7 @@ namespace WorldOfNews.Models
                 new ProposedArticle
                 {
                     ArticleID = 1,
-                    ArticleName = "Name Proposed",
+                    ArticleName = "Name Proposed 1",
                     CategoryID = 2,
                     ImagePath = "image3.png",
                     Description = "proposed article description"
@@ -119,7 +124,7 @@ namespace WorldOfNews.Models
                 new ProposedArticle
                 {
                     ArticleID = 2,
-                    ArticleName = "Name Proposed",
+                    ArticleName = "Name Proposed 2",
                     CategoryID = 4,
                     ImagePath = "image3.png",
                     Description = "proposed article description"
@@ -127,7 +132,7 @@ namespace WorldOfNews.Models
                 new ProposedArticle
                 {
                     ArticleID = 3,
-                    ArticleName = "Name Proposed",
+                    ArticleName = "Name Proposed 3",
                     CategoryID = 4,
                     ImagePath = "image3.png",
                     Description = "proposed article description"
@@ -149,6 +154,15 @@ namespace WorldOfNews.Models
                     DatePublished = DateTime.Parse("12/29/2016"),
                     ImagePath = "gigi.jpg",
                     CategoryID = 4
+                },
+                new ExternArticle
+                {
+                    ArticleID = 2,
+                    ArticleName = "Charles Manson reportedly moved from hospital back to prison",
+                    Link = "http://www.foxnews.com/us/2017/01/07/charles-manson-reportedly-moved-from-hospital-back-to-prison.html",
+                    DatePublished = DateTime.Parse("01/07/2016"),
+                    ImagePath = "charles.jpg",
+                    CategoryID = 2
                 }
             };
 
